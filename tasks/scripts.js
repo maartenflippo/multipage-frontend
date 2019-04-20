9283d67ab873f61557238c7bafa874bacc29630b
Maarten Flippo <maartenflippo@outlook.com>
2019-04-20 20:36:09 +0200

    Added babel as a default loader for webpack

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -43,6 +43,19 @@ const config = {
     output: {
         path: output.javascript,
         filename: '[name].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        ]
     }
 };
 
